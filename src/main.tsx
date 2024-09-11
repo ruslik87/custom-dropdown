58d37de60af14fff08cc5ba5b180522fe0a0e861
Ruslan Miroshnychenko <rusikm87@gmail.com>
2024-09-11 11:01:09 +0300

    add initial config

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import { store } from './app/store';
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
 );
